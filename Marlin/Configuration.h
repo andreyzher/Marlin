e1da708f256708b79a408436449580cac46cacbf
Andrey Zhereshchin <2170339+andreyzher@users.noreply.github.com>
2022-04-16 13:21:09 +0100

    Add TPU preheat settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1909,6 +1909,12 @@
 #define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED   0 // Value from 0 to 255
 
+#define PREHEAT_3_LABEL       "TPU"
+#define PREHEAT_3_TEMP_HOTEND 210
+#define PREHEAT_3_TEMP_BED     60
+#define PREHEAT_3_TEMP_CHAMBER 35
+#define PREHEAT_3_FAN_SPEED   0 // Value from 0 to 255
+
 /**
  * Nozzle Park
  *
